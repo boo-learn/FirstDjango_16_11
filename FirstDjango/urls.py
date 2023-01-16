fa7607b1dbf706baf37e7020da4b888aac37f259
boo <eyurchenko@specialist.ru>
2023-01-16 18:46:06 +0700

    Module-1 task-3 complete

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -21,4 +21,5 @@ from MainApp import views
 urlpatterns = [
     path('', views.home),
     path('about', views.about),
+    path('item/<int:id>', views.item_page),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 user_info = {
-    "name": "Евгений"
+    "name": "Евгений",
+    "surname": "Юрченко",
+    "email": "eyurchenko@specialist.ru",
+
 }
+
+items = [
+    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
+    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+    {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
+    {"id": 7, "name": "Картофель фри", "quantity": 0},
+    {"id": 8, "name": "Кепка", "quantity": 124},
+]
+
 
 # Create your views here.
 def home(request):
@@ -17,9 +29,17 @@ def home(request):
 def about(request):
     result = f"""
     Имя: <b>{user_info['name']}</b> <br>
-    Отчество: Петрович
-    Фамилия: Иванов
-    телефон: 8-923-600-01-02
-    email: vasya@mail.ru
+    Фамилия: <b>{user_info['surname']}</b> <br>
+    email: <b>{user_info['email']}</b> <br>
     """
     return HttpResponse(result)
+
+
+def item_page(request, id):
+    for item in items:
+        if item['id'] == id:
+            result = f"""
+            <h2>Название: {item['name']}</h2>
+            <div>Количество: {item['quantity']}</div>
+            """
+            return HttpResponse(result)
